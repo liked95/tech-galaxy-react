b96c91dd98d473963a00eff88754836676107097
liked95 <hoaison95.ftu@gmail.com>
2022-11-18 00:08:26 +0700

    handleSearch

--- src/components/Header/Search.jsx
+++ src/components/Header/Search.jsx
@@ -1,42 +1,82 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import magnifyingGlass from "../../assets/images/header/magnifying-glass.svg";
 import cancel from "../../assets/images/header/cancel.svg";
 import anya from "../../assets/images/contingency-images/anya-shock.gif"
 
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+
 
 
 function Search() {
+    const navigate = useNavigate()
     const [input, setInput] = useState("")
     const [focused, setFocused] = useState(false)
-    const searchResultRef = useRef(null)
 
     const onFocus = () => setFocused(true)
 
-    const onBlur = (e) => {
-        // console.log(searchResultRef.current, e.relatedTarget)
-        // Neu search-result div khong contain element dc focus sau khi search-input bi blur thi setFocus to false
-        if (!searchResultRef.current.contains(e.relatedTarget)) {
-            setFocused(false)
-        }
-    }
+    // const onBlur = (e) => setFocused(false)
+
 
 
     const handleChangeSearchInput = (e) => {
         setInput(e.target.value)
     }
 
-    const resetSearch = () => {
+    const handleClickSearchItem = () => {
         setInput("")
     }
 
+    const handleCancelSearch = () => {
+        setInput("")
+        setFocused(false)
+        inputRef.current.blur()
+    }
+
     const onKeyDown = (e) => {
+        // tab
         if (e.keyCode == 9) {
-            console.log("Tab")
+            setFocused(false)
+        }
+
+        // esc
+        if (e.keyCode == 27) {
+            handleCancelSearch()
+        }
+        // enter
+        if (e.keyCode == 13) {
+            if (renderedSearchProducts.length == 0) {
+                // setFocused(false)
+                // inputRef.current.blur()
+                return
+            }
+            const firstProduct = renderedSearchProducts[0]
+            navigate(`detail?id=${firstProduct.id}`)
+            setFocused(false)
+            setInput("")
+            inputRef.current.blur()
         }
     }
+
+    // handle Click outside
+    const inputRef = useRef(null)
+    const searchResultRef = useRef(null)
+
+    useEffect(() => {
+        const handleClickOutside = (e) => {
+            if (!inputRef.current.contains(e.target) && !searchResultRef.current.contains(e.target)) {
+                setFocused(false)
+            }
+        }
+
+        document.addEventListener('click', handleClickOutside)
+
+        return () => {
+            document.removeEventListener('click', handleClickOutside)
+        }
+    }, [])
+
 
     const products = useSelector(state => state.productList.products)
 
@@ -47,22 +87,25 @@ function Search() {
         <div className="search-input">
 
             <input type="text" placeholder="Tìm kiếm"
+                ref={inputRef}
                 value={input}
                 onChange={e => handleChangeSearchInput(e)}
                 onFocus={onFocus}
-                onBlur={onBlur}
+                onKeyDown={onKeyDown}
+            // onBlur={onBlur}
             />
             <div className="glass-img">
-                <img
+                {!input && <img
                     src={magnifyingGlass}
                     alt="magnify-glass"
                     className="magnify-glass filter-gray"
-                />
-                <img
+                />}
+                {input && <img
                     src={cancel}
                     alt="cancel"
                     className="cancel-search-icon filter-gray"
-                />
+                    onClick={handleCancelSearch}
+                />}
             </div>
             <div id="search-result" ref={searchResultRef}>
                 {focused && input && renderedSearchProducts.length > 0 && renderedSearchProducts.map((product, index) => (
@@ -70,8 +113,8 @@ function Search() {
                         to={`detail?id=${product.id}`}
                         className="search-item"
                         key={index}
-                        onClick={resetSearch}
-                        onKeyDown={onKeyDown}
+                        onClick={handleClickSearchItem}
+
                     >
                         <p>{product.name}</p>
                     </Link>
